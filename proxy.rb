6c47c4964184d0450293ec5665a8e59387d9c53f
Vinh Nguyen <xuanvinhct@gmail.com>
2019-08-04 11:31:55 +0900

    Fix redirect exception

--- proxy.rb
+++ proxy.rb
@@ -8,11 +8,14 @@ end
 get '/' do
   url = params[:path]
   user_agent = request.env['HTTP_USER_AGENT']
-  ProxyHandler.http(url, {'User-Agent' => user_agent})
+  ProxyHandler.http(url, {'User-Agent' => user_agent, redirect: false})
 end
 
 class ProxyHandler
   def self.http(url, options = {})
     open(url, options)
+  rescue OpenURI::HTTPRedirect => e
+    url = e.uri
+    retry
   end
 end
